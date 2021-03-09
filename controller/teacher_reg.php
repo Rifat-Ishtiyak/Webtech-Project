92518c548d2d9de69635951201ea137a7329d7c4
Rifat-Ishtiyak <rifatishtiyak@gmail.com>
2021-03-09 17:35:06 +0600

    image validaton added

--- controller/teacher_reg.php
+++ controller/teacher_reg.php
@@ -4,41 +4,54 @@
             echo"Plaese fill out all the field";
         }
         else{
+            //validation check
 
-            $name= $_POST['name'];
-            $id=$_POST['id'];
-            $designation=$_POST['desig'];
-            $email= $_POST['email'];
-            $phone = $_POST['phone'];
-            $address=$_POST['address'];
-            $pass = $_POST['pass'];
-            $cPass=$_POST['cPass'];
-            $dob=$_POST['dob'];
-            move_uploaded_file($_FILES['profilePic']['tmp_name'], '../assets/'.$_FILES['profilePic']['name']);
-            $profilePic='../assets/'.$_FILES['profilePic']['name'];
-            $bloodGroop=$_POST['bloodGroup'];
-            $qualification=$_POST['qualification'];
+            $target_file = basename($_FILES["profilePic"]["name"]);
+            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-            $user=['name'=> $name, 'id'=>$id, 'desig'=>$designation,'email'=> $email, 'bloodGroup'=> $bloodGroop , 'phone'=>$phone, 'address'=>$address, 'pass'=>$pass, 'dob'=>$dob, 'profilePic'=>$profilePic, 'qualification'=>$qualification ];
-
-            if ($pass!=$cPass) {
-                echo"please check your password and confirm password";
+            if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+                echo "only jpeg, jpg & png files are allowed.";
             }
             else{
-                if(filesize('../model/teacher.json')==0){
-					$json=json_encode($user);
-					$myfile=fopen('../model/teacher.json', 'w');
-					fwrite($myfile, '['.$json.']');
-				}
-				else{
-					$inp = file_get_contents('../model/teacher.json');
-					$tempArray = json_decode($inp);
-					array_push($tempArray, $user);
-					$jsonData = json_encode($tempArray);
-					file_put_contents('../model/teacher.json', $jsonData);
-				}
-				header('location: ../view/login.html');
+
+                $name= $_POST['name'];
+                $id=$_POST['id'];
+                $designation=$_POST['desig'];
+                $email= $_POST['email'];
+                $phone = $_POST['phone'];
+                $address=$_POST['address'];
+                $pass = $_POST['pass'];
+                $cPass=$_POST['cPass'];
+                $dob=$_POST['dob'];
+                move_uploaded_file($_FILES['profilePic']['tmp_name'], '../assets/'.$_FILES['profilePic']['name']);
+                $profilePic='../assets/'.$_FILES['profilePic']['name'];
+                $bloodGroop=$_POST['bloodGroup'];
+                $qualification=$_POST['qualification'];
+
+                $user=['name'=> $name, 'id'=>$id, 'desig'=>$designation,'email'=> $email, 'bloodGroup'=> $bloodGroop , 'phone'=>$phone, 'address'=>$address, 'pass'=>$pass, 'dob'=>$dob, 'profilePic'=>$profilePic, 'qualification'=>$qualification ];
+
+                if ($pass!=$cPass) {
+                    echo"please check your password and confirm password";
+                }
+                else{
+                    if(filesize('../model/teacher.json')==0){
+                        $json=json_encode($user);
+                        $myfile=fopen('../model/teacher.json', 'w');
+                        fwrite($myfile, '['.$json.']');
+                    }
+                    else{
+                        $inp = file_get_contents('../model/teacher.json');
+                        $tempArray = json_decode($inp);
+                        array_push($tempArray, $user);
+                        $jsonData = json_encode($tempArray);
+                        file_put_contents('../model/teacher.json', $jsonData);
+                    }
+                    header('location: ../view/login.html');
+                }
+
             }
+
+            
         }
     }
 
